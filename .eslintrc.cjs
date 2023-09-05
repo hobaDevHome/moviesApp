b6f1c192879ea36fabbc3ede7a59fb157b7111c8
Heba <heba96@yahoo.com>
2023-09-05 11:50:57 +0300

    eslint package adding

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,36 @@
-module.exports = {
-  env: { browser: true, es2020: true },
+{
+  // Specify the environment where your code will run (e.g., browser, node, etc.).
+  parser: "babel-eslint",
+  "env": {
+    "browser": true,
+    es2021: true,
+  },
+
+  // Extend a predefined ESLint configuration.
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
+    "eslint:recommended", // Use ESLint's recommended rules.
+    "plugin:react/recommended", // Use recommended React rules.
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "airbnb", // Use Airbnb's style guide.
   ],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+
+  // Define parser options.
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true, // Enable JSX parsing.
+    },
+    ecmaVersion: "latest", // Specify the ECMAScript version (ES2021).
+    sourceType: "module", // Use ES6 modules.
+  },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+ 
+  // Define custom rules or override existing rules here.
   rules: {
     "react-refresh/only-export-components": "warn",
     "react/prop-types": "off",
+    "react/jsx-no-target-blank": "error",
   },
-};
+}
+
+
